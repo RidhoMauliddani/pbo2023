a82a465966fc251128dccb025e2bb7768ce88e02
Ridho Mauliddani <112556943+Ridhom27@users.noreply.github.com>
2023-10-17 22:19:10 +0700

    Add files via upload

---
+++ pertemuan 1/membuat limas segitiga.py
@@ -0,0 +1,31 @@
+"""nama : Ridho Mauliddani
+NIM  : 220511101
+kelas: TIF22E/R5"""
+
+print("mencari limas segitiga")
+
+#variabel
+ls1      = 10
+ls2      = 10
+ls3      = 10
+ls4      = 10
+alas     = 10
+Tinggi_L = 10
+tinggi_S = 10
+
+# rumus
+luas   = (ls1 + ls2 + ls3 + ls4)
+volume =(1/6) * alas * tinggi_S * Tinggi_L
+
+#print out
+print("ls1 :", ls1)
+print("ls2 :", ls2)
+print("ls3 :", ls3)
+print("ls4 :", ls4)
+print("alas:", alas)
+print("Tinggi_L :", Tinggi_L)
+print("tinggi_S:", tinggi_S)
+print("luas :", luas)
+print("volume :", volume)
+
+
