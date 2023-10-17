33d2be32080e917c866e30a347c129853dd48341
Ridho Mauliddani <112556943+Ridhom27@users.noreply.github.com>
2023-10-17 20:32:39 +0700

    Add files via upload

---
+++ pertemuan 1/membuat limas segi empat.py
@@ -0,0 +1,27 @@
+"""nama : Ridho Mauliddani
+NIM  : 220511101
+kelas: TIF22E/R5"""
+
+print("mencari rumus limas segiempat")
+
+# variabel
+luas_s1 = 15
+luas_s2 = 15
+luas_s3 = 15
+luas_s4 = 15
+luas_s5 = 15
+luas_a  = 30
+tinggi  = 15
+
+# rumus 
+luas_limas_segiempat = luas_s1 + luas_s2 + luas_s3 + luas_s4 + luas_s5
+volume = (1/3) * luas_a * tinggi
+
+# print_out
+print("luas_s1 :", luas_s1)
+print("luas_s2 :", luas_s2)
+print("luas_s3 :", luas_s3)
+print("luas_s4 :", luas_s4)
+print("luas_s5 :", luas_s5)
+print("luas limas segiempat:", luas_limas_segiempat)
+print("volume:", volume)
