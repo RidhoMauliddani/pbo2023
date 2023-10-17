87b54e7fc27237130b175461ce673419a5592245
Ridho Mauliddani <112556943+Ridhom27@users.noreply.github.com>
2023-10-17 17:39:52 +0700

    Add files via upload

---
+++ pertemuan 1/mencari kubus.py
@@ -0,0 +1,22 @@
+"""nama  = Ridho Mauliddani
+   kelas = TIF22E
+   NIM   = 220511101"""
+
+print("membuat rumus kubus")
+
+# variabel
+rusuk  = 15
+rusuk  = 15
+rusuk  = 15
+
+# rumus
+luas   = 6 * rusuk  * rusuk 
+volume = rusuk  * rusuk  * rusuk 
+
+# print out
+
+print("rusuk : ", rusuk)
+print("rusuk : ", rusuk)
+print("rusuk : ", rusuk)
+print("luas:", luas)
+print("volume:", volume)
