887cad071d667ee14e7fa1af49e5fc2dfcbf6502
Ridho Mauliddani <112556943+RidhoMauliddani@users.noreply.github.com>
2023-10-23 15:40:46 +0700

    Add files via upload

---
+++ Pertemuan1 /Program Luas Dan Volume/limas segi empat.py
@@ -0,0 +1,18 @@
+"""nama : Ridho Mauliddani
+NIM  : 220511101
+kelas: TIF22E/R5"""
+
+print("mencari rumus limas segiempat")
+
+# variabel
+luas_alas = 10
+luas_sisi = 6
+tinggi_limas = 8
+
+# rumus
+luas_permukaan = luas_alas + luas_sisi
+volume = 1/3 * luas_alas * tinggi_limas
+
+#output
+print("luas permukaan :", luas_permukaan)
+print("volume : ", volume)
